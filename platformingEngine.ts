92c4b571bc164b5bbcfe674372e65b08548c10a3
luke-e-gibson-school <113417958+luke-e-gibson-school@users.noreply.github.com>
2022-12-29 12:47:34 +1100

    Initial files for MakeCode project

---
+++ platformingEngine.ts
@@ -0,0 +1,24 @@
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11
+namespace Platfrom {
+    /**
+     * A Simple Platfroming Engine
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+
+    class PlayerCharacter { }
+
+    //% block
+    export function f(): void {
+        // Add code here
+    }
+    //% block
+    export const PlayerController = new PlayerCharacter()
+
+    
+
+}
